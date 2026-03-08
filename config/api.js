f28f4d545c814bb119111dba1b3b441ef181712f
Abdelmneimmm <abdelmneimmm@gmail.com>
2026-03-08 14:02:35 +0200

    fix web build

--- config/api.js
+++ config/api.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
-// غيّر الـ IP ده لـ IP اللابتوب بتاعكconst BASE_URL = 'https://crm-server-three-blond.vercel.app';
+var serverUrl = 'https://crm-server-three-blond.vercel.app';
 
-const api = axios.create({
-  baseURL: BASE_URL,
+var api = axios.create({
+  baseURL: serverUrl,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-let authToken = null;
+var authToken = null;
 
-export const setToken = (token) => {
+export var setToken = function(token) {
   authToken = token;
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 };
 
-export const clearToken = () => {
+export var clearToken = function() {
   authToken = null;
   delete api.defaults.headers.common['Authorization'];
 };
 
-export const getToken = () => authToken;
+export var getToken = function() { return authToken; };
 
 export default api;
